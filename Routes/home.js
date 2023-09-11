d71450968f6fd040e8257919272f31e357bfa4c7
aorko01 <2105149@ugrad.cse.buet.ac.bd>
2023-09-11 15:05:36 +0600

    dgsgf

--- Routes/home.js
+++ Routes/home.js
@@ -153,6 +153,101 @@ router.get('/category/:category', async(req, res) => {
   }
 })
 
+router.get('/updateSearchList/farmname', async(req, res) => {
+  const client = await pool.connect();
+  const r = await client.query('SELECT farm_name FROM Cattle_Advertisement');
+  client.release(true);
+  res.send(r.rows);
+})
+
+router.get('/updateSearchList/sellername', async(req, res) => {
+const client = await pool.connect();
+const r = await client.query('SELECT name FROM Users natural join Seller');
+client.release(true);
+res.send(r.rows);
+})
+
+router.post('/search/sellername', async(req, res) => {
+  var {search} = req.body;
+  if(req.cookies['thana']){
+    const thana = req.cookies['thana']
+    const client = await pool.connect();
+    const seller_id = await client.query("SELECT user_id FROM Users where name=$1",[search]);
+    if(seller_id.rows.length!=0){
+      const meatAdvertisements = await client.query("select * from (SELECT thana.name,users.user_id FROM thana join Users on thana.thana_id=users.thana_id)as c join (select * from advertisements JOIN meat_advertisement on advertisements.advertise_id=meat_advertisement.advertise_id) as a on c.user_id= a.seller_id where a.seller_id IN(SELECT user_id FROM Users where name=$1) AND c.name=$2 AND a.verified=true",[search,thana]);
+      const cattleAdvertisements = await client.query("select * from (SELECT thana.name,users.user_id FROM thana join Users on thana.thana_id=users.thana_id)as c join (SELECT * FROM advertisements Natural JOIN ( SELECT * FROM cattle_advertisement JOIN ( SELECT * FROM cattle WHERE cattle_id IN (SELECT MIN(cattle_id)  FROM cattle GROUP BY cattle_advertise_id) ) AS x ON x.cattle_advertise_id = cattle_advertisement.advertise_id ) AS y) as a on c.user_id= a.seller_id where a.seller_id IN(SELECT user_id FROM Users where name=$1) AND c.name=$2 AND a.verified=true",[search,thana]);
+      const rawhideAdvertisements = await client.query("select * from (SELECT thana.name,users.user_id FROM thana join Users on thana.thana_id=users.thana_id)as c join (SELECT * FROM advertisements Natural JOIN rawhide_advertisement) as a on c.user_id= a.seller_id where a.seller_id IN(SELECT user_id FROM Users where name=$1) AND c.name=$2 AND a.verified=true",[search,thana]);
+      const hornAdvertisements = await client.query("select * from (SELECT thana.name,users.user_id FROM thana join Users on thana.thana_id=users.thana_id)as c join (SELECT * FROM advertisements Natural JOIN horn_advertisement) as a on c.user_id= a.seller_id where a.seller_id IN(SELECT user_id FROM Users where name=$1) AND c.name=$2 AND a.verified=true",[search,thana]);
+      const hoofAdvertisements = await client.query("select * from (SELECT thana.name,users.user_id FROM thana join Users on thana.thana_id=users.thana_id)as c join (SELECT * FROM advertisements Natural JOIN hoof_advertisement) as a on c.user_id= a.seller_id where a.seller_id IN(SELECT user_id FROM Users where name=$1) AND c.name=$2 AND a.verified=true",[search,thana]);
+      client.release(true);
+      var bestMeat = undefined;
+      var bestCattle = undefined;
+      for(var i=0;i<meatAdvertisements.rows.length;i++){
+          meatAdvertisements.rows[i].created_at=formatDate(JSON.stringify(meatAdvertisements.rows[i].created_at).split('T')[0].split('"')[1]);
+      }
+      console.log(meatAdvertisements.rows)
+      for(var i=0;i<cattleAdvertisements.rows.length;i++){
+          cattleAdvertisements.rows[i].created_at=formatDate(JSON.stringify(cattleAdvertisements.rows[i].created_at).split('T')[0].split('"')[1]);
+      }
+      for(var i=0;i<rawhideAdvertisements.rows.length;i++){
+          rawhideAdvertisements.rows[i].created_at=formatDate(JSON.stringify(rawhideAdvertisements.rows[i].created_at).split('T')[0].split('"')[1]);
+          rawhideAdvertisements.rows[i].date_of_storage=formatDate(JSON.stringify(rawhideAdvertisements.rows[i].date_of_storage).split('T')[0].split('"')[1]);
+      }
+      for(var i=0;i<hornAdvertisements.rows.length;i++){
+          hornAdvertisements.rows[i].created_at=formatDate(JSON.stringify(hornAdvertisements.rows[i].created_at).split('T')[0].split('"')[1]);
+          hornAdvertisements.rows[i].date_of_storage=formatDate(JSON.stringify(hornAdvertisements.rows[i].date_of_storage).split('T')[0].split('"')[1]);
+      }
+      for(var i=0;i<hoofAdvertisements.rows.length;i++){
+          hoofAdvertisements.rows[i].created_at=formatDate(JSON.stringify(hoofAdvertisements.rows[i].created_at).split('T')[0].split('"')[1]);
+          hoofAdvertisements.rows[i].date_of_storage=formatDate(JSON.stringify(hoofAdvertisements.rows[i].date_of_storage).split('T')[0].split('"')[1]);
+      }
+      res.render('homepage',{session:req.session.phone_number,thana:thana,type:req.session.type,meatAdvertisements:meatAdvertisements.rows,cattleAdvertisements:cattleAdvertisements.rows,rawhideAdvertisements:rawhideAdvertisements.rows,hornAdvertisements:hornAdvertisements.rows,hoofAdvertisements:hoofAdvertisements.rows,bestMeat:bestMeat,bestCattle:bestCattle}) 
+    }
+    else{
+      res.render('homepage',{session:req.session.phone_number,thana:thana,type:req.session.type,meatAdvertisements:[],cattleAdvertisements:[],rawhideAdvertisements:[],hornAdvertisements:[],hoofAdvertisements:[],bestMeat:undefined,bestCattle:undefined})
+    }
+  }
+  else{
+    res.redirect('/coordinates')
+  }
+})
+
+router.post('/search/farmname', async(req, res) => {
+  var {search} = req.body;
+  if(req.cookies['thana']){
+    const thana = req.cookies['thana']
+    const client = await pool.connect();
+    const cattleAdvertisements = await client.query("select * from (SELECT thana.name,users.user_id FROM thana join Users on thana.thana_id=users.thana_id)as c join (SELECT * FROM advertisements Natural JOIN ( SELECT * FROM cattle_advertisement JOIN ( SELECT * FROM cattle WHERE cattle_id IN (SELECT MIN(cattle_id)  FROM cattle GROUP BY cattle_advertise_id) ) AS x ON x.cattle_advertise_id = cattle_advertisement.advertise_id WHERE cattle_advertisement.farm_name=$1 ) AS y) as a on c.user_id= a.seller_id where c.name=$2 AND a.verified=true",[search,thana]);
+    client.release(true);
+    for(var i=0;i<cattleAdvertisements.rows.length;i++){
+      cattleAdvertisements.rows[i].created_at=formatDate(JSON.stringify(cattleAdvertisements.rows[i].created_at).split('T')[0].split('"')[1]);
+    }
+    
+    res.render('homeByCategory',{session:req.session.phone_number,type:req.session.type,allAdvertisements:cattleAdvertisements.rows}) 
+  }
+  else{
+    res.redirect('/coordinates')
+  }
+})
+
+router.post('/search/meatsubtype', async(req, res) => {
+  var {search} = req.body;
+  if(req.cookies['thana']){
+      const thana = req.cookies['thana']
+      const client = await pool.connect();
+      const meatAdvertisements = await client.query("select * from (SELECT thana.name,users.user_id FROM thana join Users on thana.thana_id=users.thana_id)as c join (select * from advertisements JOIN meat_advertisement on advertisements.advertise_id=meat_advertisement.advertise_id where meat_advertisement.type=$1) as a on c.user_id= a.seller_id where c.name=$2 AND a.verified=true",[search,thana]);
+      client.release(true);
+      for(var i=0;i<meatAdvertisements.rows.length;i++){
+        meatAdvertisements.rows[i].created_at=formatDate(JSON.stringify(meatAdvertisements.rows[i].created_at).split('T')[0].split('"')[1]);
+      }
+      console.log(meatAdvertisements.rows)
+      res.render('homeByCategory',{session:req.session.phone_number,type:req.session.type,allAdvertisements:meatAdvertisements.rows}) 
+  }
+  else{
+    res.redirect('/coordinates')
+  }
+})
+
 router.get('/search', async(req, res) => {
   res.render('singleAdd', {type1:"1", type2:"5"})
 })
---
+++ public/scripts/home.js
@@ -0,0 +1,99 @@
+var type
+function optionHandler(a){
+    if(a==1){
+        document.getElementById("searchID").innerHTML = "Farmname";
+        document.getElementById("form").setAttribute("action", "/search/farmname");
+    }
+    else if(a==2){
+        document.getElementById("searchID").innerHTML = "Seller name";
+        document.getElementById("form").setAttribute("action", "/search/sellername");
+    }
+    else if(a==3){
+        document.getElementById("searchID").innerHTML = "Meat subtype";
+        document.getElementById("form").setAttribute("action", "/search/meatsubtype");
+    }
+    type = a;
+    updateSearchList(type);
+}
+
+let arr =[]
+var searchBar = document.getElementById('search');
+var results = document.querySelector('.results');
+
+const searchHandler = ()=>{
+    var input = searchBar.value;
+    if(input!=""){
+        var suggestions = arr.filter((data)=>{
+            return data.toLowerCase().startsWith(input.toLowerCase());
+        })
+        if(suggestions.length!=0){
+            var html = suggestions.map((data)=>{
+                return `<li onclick="clickHandler(this)">${data}</li>`
+            })
+            results.innerHTML =  `<ul>${html}</ul>`;
+        }
+        else{
+            results.innerHTML = '';
+        }
+    }
+    else{
+        results.innerHTML = '';
+    }
+}
+const clickHandler = (obj)=>{
+    searchBar.value=obj.innerHTML;
+    results.innerHTML='';
+}
+
+async function updateSearchList(type){
+    if(type==1){
+        fetch('/updateSearchList/farmname', {
+            method: 'GET'
+        })
+        .then(res => {
+            if (!res.ok) {
+              throw new Error('Network response status was vejal');
+            }
+            return res.json();
+        })
+        .then(data => {
+            arr = [];
+            for(let i = 0; i < data.length; i++){
+                arr.push(data[i].farm_name);
+            }
+            // districts.sort()
+        })
+        .catch(err => {
+            console.log(err.Error);
+        });
+    }
+    else if(type==2){
+        fetch('/updateSearchList/sellername', {
+            method: 'GET'
+        })
+        .then(res => {
+            if (!res.ok) {
+              throw new Error('Network response status was vejal');
+            }
+            return res.json();
+        })
+        .then(data => {
+            arr = [];
+            for(let i = 0; i < data.length; i++){
+                arr.push(data[i].name);
+            }
+            // districts.sort()
+        })
+        .catch(err => {
+            console.log(err.Error);
+        });
+    }
+    else if(type==3){
+        arr = ["Rib steak","Rib eye steak","Tri-Trip Roast","T-bone steak","Rump cap","Round cut","Shank Meat","Flank steak","Short plate primal","Foreshank","Brisket cut","Beef chuck"] 
+    }
+}
+
+function formHandler(e){
+    e.preventDefault();
+    console.log("form submitted");
+}
